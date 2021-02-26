57cc2fc1196f003771fb8d70f398a88ef3525c0c
Jeff Tan <jefftan969@gmail.com>
2021-02-26 00:21:09 -0500

    add map click functionality

--- src/components/PointCloud/PointCloud.jsx
+++ src/components/PointCloud/PointCloud.jsx
@@ -39,7 +39,58 @@ function PointCloud() {
             topic: Constants.POINTCLOUD_TOPIC,
             material: { size: 0.01, color: 0xaaaaaa },
             max_pts: 10000000
-        })
+        });
+
+        // Topic for publishing robot commands
+        var cmdPos = new ROSLIB.Topic({
+            ros: ros,
+            name: Constants.COMMAND_TOPIC,
+            messageType: "geometry_msgs/Point"
+        });
+
+        viewer.cameraControls.addEventListener("mousedown", (event3D) => {
+            var domEvent = event3D.domEvent;
+            domEvent.preventDefault();
+            console.log("Mouse event: ", event3D);
+
+            // pos_x and pos_y are between -1 and 1, specified within the
+            // rectangular bounding box of the viewer frame
+            //var posX = event3D.mousePos.x;
+            //var posY = event3D.mousePos.y;
+
+            switch (domEvent.button) {
+                case 0:
+                    // Left mouse button
+                    var origin = event3D.mouseRay.origin;
+                    var direction = event3D.mouseRay.direction;
+
+                    // Find where mouse click intersects with the z=0 plane
+                    // Alternatively, may want to use event3D.currentTarget and
+                    // event3D.intersection to find nearest point on pointcloud
+                    var scaleFactor = origin.z / direction.z;
+                    var x = origin.x - scaleFactor * direction.x;
+                    var y = origin.y - scaleFactor * direction.y;
+                    var z = origin.z - scaleFactor * direction.z; // should be 0
+                    console.log("Mouse down: X ", x, ", Y ", y, ", Z ", z);
+                
+                    // Send position command to robot
+                    var position = new ROSLIB.Message({
+                        x: x,
+                        y: y,
+                        z: z
+                    });
+                    cmdPos.publish(position);
+                    break;
+
+                case 1:
+                    // Middle mouse button
+                    break;
+
+                case 2:
+                    // Right mouse button
+                    break;
+            }
+        });
     })
 
     return (
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 export const WEBSOCKET_URL = "ws://eracer.local:9090";
 export const POINTCLOUD_TOPIC = "/realsense/camera/depth/color/points";
+export const COMMAND_TOPIC = "/fake/topic/lmao";
 export const TF_NAME = "/camera_link";
